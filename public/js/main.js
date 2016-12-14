a6c0d122732b49e47a35f88b4b6aff2af9e89bf6
briandridge <briandridge@gmail.com>
2016-12-14 10:56:13 -0700

    makes search and append results work

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,47 @@
 
-// get request  - search for artist, get the id of that artist, get that artist's related artists
-$(document).ready(function(){
+// search for artist, append search results to dom
+$(document).ready(function() {
 	$("#getRelatedArtists").on("submit", function(){
 		event.preventDefault();
-		// for Each
 		var searchArtist = $("#searchArtist").val();
 		$.get('https://api.spotify.com/v1/search?q=' + searchArtist + '&type=artist').done(function(req, res) {
-			var name = req.artists.items[0].name;
-			var image = req.artists.items[0].images[3].url;
-			console.log(req);
-			$("#results").append("<li>" + name + "</li>");
-			// console.log("response?"+response);
-			// get artist id
-			// $.get('https://api.spotify.com/v1/artists/' + resObj.artists.items.id + '/related-artists').done(function(response) {
-			// 	// var $sentence = jQuery.parseJSON($sentenceResults.responseText);
-			// $("#results").append("<li>" + response.artists.items.id + "</li>");
+			var resultsArr = req.artists.items;
+			console.log(resultsArr);
+			for (var i = 0; i < resultsArr.length; i++) {
+				// resArr.push({name: resultsArr[i].name, id: resultsArr[i].id});
+				$("#searchResults").append("<div class='col-md-3'><input class= 'btn btn-default' id='" + resultsArr[i].id +"'type= 'submit' value=" + resultsArr[i].name+ "></div>");
+				// $("#results").append("<li>" + artistItem[i].images[3].url + "</li>");
+			}
+			return resultsArr;
 		});
 	});
-
-	// 	var $results = $.get('https://api.spotify.com/v1/artists/'+ $searchArtist + '3kSqc2brwAF1kWRFWYe2fW/related-artists').done(function(response){
-	// 		// var $sentence = jQuery.parseJSON($sentenceResults.responseText);
-	// 		$("#results").append("<li>" + response.name + "</li>");
-	// 		console.log($results);
-	// 		// push res back end db
-	// 		// $.post to bakend
-	// 	});
-	// });
 });
+
+$(document).ready(function() {
+	$("#searchResults").on("submit", function() {
+		event.preventDefault();
+		console.log("preventDefault");
+		var chosenArtistId = $(this).attr("id");
+		console.log(chosenArtistId);
+		$.get('https://api.spotify.com/v1/artists' + chosenArtistId).done(function() {
+			$("#chosenArtist").append("<div class='col-md-12'><input class= 'btn btn-default' id='" + chosenArtistId +"'type= 'submit' value=" + chosenArtistId + "></div>");
+		});
+		$.get('https://api.spotify.com/v1/artists' + chosenArtistId + '/related-artists').done(function() {
+			$("#chosenArtistRelated").append("<div class='col-md-3'><input class= 'btn btn-default' id='" + chosenArtistId +"'type= 'submit' value=" + chosenArtistId + "></div>");
+		});
+	});
+});
+
+// // on click, 
+// make search results dissapear
+// reset search box
+// get artist related artists, 
+// display artist,
+// display related artist
+
+
+// $.get('https://api.spotify.com/v1/artists/' + resObj.artists.items.id + '/related-artists').done(function(response) {
+			// $("#results").append("<li>" + response.artists.items.id + "</li>");
 
 
 
