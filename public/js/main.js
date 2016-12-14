7fd82e1e5c6f927d76c5e2b01919279f99466363
briandridge <briandridge@gmail.com>
2016-12-14 13:32:10 -0700

    working on heroku

--- public/js/main.js
+++ public/js/main.js
@@ -9,19 +9,15 @@ $(document).ready(function() {
 			console.log(resultsArr);
 			for (var i = 0; i < resultsArr.length; i++) {
 				// resArr.push({name: resultsArr[i].name, id: resultsArr[i].id});
-				$("#searchResults").append("<div class='col-md-3'><input class= 'btn btn-default' id='" + resultsArr[i].id +"'type= 'submit' value=" + resultsArr[i].name+ "></div>");
+				$("#searchResults").append("<div class='col-xs-12 col-sm-6 col-md-3'><input class= 'btn btn-default searchResults' id='" + resultsArr[i].id +"'type= 'submit' value=" + resultsArr[i].name + "> </div>");
 				// $("#results").append("<li>" + artistItem[i].images[3].url + "</li>");
 			}
-			return resultsArr;
 		});
 	});
-});
-
-$(document).ready(function() {
 	$("#searchResults").on("submit", function() {
 		event.preventDefault();
 		console.log("preventDefault");
-		var chosenArtistId = $(this).attr("id");
+		var chosenArtistId = $this.id;
 		console.log(chosenArtistId);
 		$.get('https://api.spotify.com/v1/artists' + chosenArtistId).done(function() {
 			$("#chosenArtist").append("<div class='col-md-12'><input class= 'btn btn-default' id='" + chosenArtistId +"'type= 'submit' value=" + chosenArtistId + "></div>");
@@ -31,6 +27,21 @@ $(document).ready(function() {
 		});
 	});
 });
+
+// $(document).ready(function() {
+// 	$("#searchResults").on("submit", function() {
+// 		event.preventDefault();
+// 		console.log("preventDefault");
+// 		var chosenArtistId = $(this).attr("id");
+// 		console.log(chosenArtistId);
+// 		$.get('https://api.spotify.com/v1/artists' + chosenArtistId).done(function() {
+// 			$("#chosenArtist").append("<div class='col-md-12'><input class= 'btn btn-default' id='" + chosenArtistId +"'type= 'submit' value=" + chosenArtistId + "></div>");
+// 		});
+// 		$.get('https://api.spotify.com/v1/artists' + chosenArtistId + '/related-artists').done(function() {
+// 			$("#chosenArtistRelated").append("<div class='col-md-3'><input class= 'btn btn-default' id='" + chosenArtistId +"'type= 'submit' value=" + chosenArtistId + "></div>");
+// 		});
+// 	});
+// });
 
 // // on click, 
 // make search results dissapear
