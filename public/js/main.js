0b8ca8ad4f27b37a8adc81e25ed22a7d0d3bfda5
briandridge <briandridge@gmail.com>
2016-12-15 10:16:21 -0700

    changes button for div

--- public/js/main.js
+++ public/js/main.js
@@ -6,15 +6,16 @@ $(document).ready(function() {
 		var searchArtist = $("#searchArtist").val();
 		$.get('https://api.spotify.com/v1/search?q=' + searchArtist + '&type=artist').done(function(req, res) {
 			var resultsArr = req.artists.items;
-			console.log(resultsArr[0].name);
+			// var image = resultsArr[i].images
+			console.log(resultsArr[0]);
 			for (var i = 0; i < resultsArr.length; i++) {
-				$("#searchResultsRow").append("<div class='col-xs-12 col-sm-6 col-md-3'><input onclick= 'chooseArtist()' class= 'btn btn-default searchResultsItem' id='" + resultsArr[i].id +"'type= 'submit' value=" + resultsArr[i].name + "> </div>");
-				console.log(resultsArr[i].name);
+				$("#searchResultsRow").append("<div class='col-xs-12 col-sm-6 col-md-3 target' id='" + resultsArr[i].id + "' onclick= 'chooseArtist()'>"  + resultsArr[i].name + "</div>");
+				// + "<img src='" + resultsArr[i].images[3].url + "'  alt='artist thumbnail'>"
+				// console.log(resultsArr[i].images[3].url);
 			}
 			var chooseArtist = function() {
 				console.log("click works");
 				console.log(resultsArr[i].id);
-				// console.log(resultsArr[i].id);
 				$.get('https://api.spotify.com/v1/artists' + resultsArr[i].id).done(function() {
 					$("#chosenArtist").append("<div class='col-md-12'><input class= 'btn btn-default' id='" + resultsArr[i].id +"'type= 'submit' value=" + resultsArr[i].id + "></div>");
 				});
